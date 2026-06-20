591988252ff0e4ec29d127d656bd0a3f82409805
Soysakiaytuno <leonardhopst@gmail.com>
2026-06-19 20:19:06 -0400

    test: [HU-01] agregar test para Dado que quiero ver la lista de pacientes / Cuando entre a la lista / Entonces se debe visualizar los datos basicos de la consulta

---
+++ src/validacion.listado.test.js
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest';
+import { cargarCita } from './listado.js';
+describe('Validación de visualizacion de la lista de pacientes', () => {
+
+    it('Happy Path: Mostrar una lista de pacientes mostrando datos basicos de previsualizacion', () => {
+        const citaMock =
+        {
+            title: 'Consulta General - Juan Perez',
+            start_time: '2026-11-05T10:00:00',
+        };
+        const resultado = cargarCita(citaMock);
+        expect(resultado).toContain('Consulta General - Juan Perez');
+        expect(resultado).toContain('DOMINGO, 5 DE NOVIEMBRE DE 2026, 10:00');
+    });
+
+    it('Negative Path: No muestra los pacientes o no muestra la informacion completa', () => {
+        const citaMock =
+        {
+            title: '',
+            start_time: '2026-11-05T10:00:00',
+        };
+        const resultado = cargarCita(citaMock);
+        expect(resultado).toContain('Consulta sin titulo');
+    });
+});
